8dc297b9c99a74437f451b4811373988f9a09322
Lucca <lucca.anderle1512@gmail.com>
2025-05-09 11:05:22 -0300

    Mudança da vizualização dos clientes para o formato de tabela

--- aula09/clientes.php
+++ aula09/clientes.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 3px;
+        }
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 09 - Clientes Cadastrados</title>
@@ -40,12 +47,27 @@
         // Enquanto houver registros armazenados em 'resultado', vamos criar um array associativo para cada registro retornado.
         // Mostraremos na tela, o array associativo a cada iteração do laço.
         echo "<h2>Clientes Cadastrados:</h2>";
+
+        // Montar o cabçalho da tabela
+        echo "<table>";
+        echo    "<tr>";
+        echo        "<th>ID</th>";
+        echo        "<th>Nome</th>";
+        echo        "<th>Telefone</th>";
+        echo        "<th>E-mail</th>";
+        echo    "</tr>";
+
         while($cliente = mysqli_fetch_assoc($resultado)) {
-            echo "ID: "         . $cliente['id']    . "<br>";
-            echo "Nome: "       . $cliente['nome']  . "<br>";
-            echo "Telefone: "   . $cliente['fone']  . "<br>";
-            echo "E-mail: "     . $cliente['email'] . "<br>";
+            echo "<tr>";
+            echo    "<td>" . $cliente['id']    . "</td>";
+            echo    "<td>" . $cliente['nome']  . "</td>";
+            echo    "<td>" . $cliente['fone']  . "</td>";
+            echo    "<td>" . $cliente['email'] . "</td>";
+            echo "</tr>";
         }
+
+        // Fecha a tabela
+        echo "</table>";
 
         mysqli_close($conn); // Encerra a conexão com o banco
     ?>
